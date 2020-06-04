e41edc47f4fc358e99ea31af7884b41b61ee4d6a
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-06-04 23:50:11 +0100

    Remove Normalizer type

--- cmd/root.go
+++ cmd/root.go
@@ -27,7 +27,6 @@ import (
 
 var cfgFile string
 var outputFile string
-type Normalizer func(class interface{}) map[string]string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
